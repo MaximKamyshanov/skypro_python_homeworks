6303a40e656b1428526fa34ea580256c829f731b
MaximKamyshanov <kamyshanovmaxim@gmail.com>
2024-06-14 16:20:22 +0300

    Home_python_1

--- Lesson1/lesson_1_task_4.py
+++ Lesson1/lesson_1_task_4.py
@@ -1,4 +1,3 @@
 first_name = input("Как вас зовут? ")
 last_name = input("Какая ваша фамилия? ")
-x = input()
-print("Вас зовут: " + first_name + last_name)
+print("Вас зовут: " + first_name, last_name)
